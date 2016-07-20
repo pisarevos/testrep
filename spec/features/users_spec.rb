a8e9c8bb955302071e7eb7b7d37556a628192885
Anton Petrunich <anton.petrunich@gmail.com>
2016-07-20 16:58:28 +0300

    Test enronment setup

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe '/users' do
+  before do
+    visit '/users'
+  end
+
+  it 'has correct title' do
+    expect(page).to have_css("h1.page-title", text: "Users")
+  end
+
+  it 'render all users' do
+    expect(page).to have_css("h1.page-title", text: "Users")
+  end
+end
